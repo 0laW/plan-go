9aa322edb34c1e05f1cf960581fd5a1fe6bede32
Aleksandra Wilk <aleksandra_wilk@yahoo.com>
2025-05-27 18:18:12 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
   root to: "trips#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_for :users
+
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :users, only: [:show, :edit, :update] do
-    resources :preferences, only: [:create]
+  resources :users, only: [:show, :edit, :update, :index]
+  resources :preferences, only: [:new, :create]
+
+  resources :trips, only: [:new, :create, :show] do # location/budget/dates
+    resources :trip_activities [:create]
   end
 
-  resources :trips, only: [:create] do # location/budget/dates
-    resources :trip_users  # people going on the trip
-    resources :trip_activities  # itinerary
-  end
+  resources :trip_activities [:destroy]
 
-  resources :activities, only: [:show] # activities that will be in the itinerary
-  resources :activity_reviews, only: [:create]
+  resources :activities do
+    resources :activity_reviews, only: [:create]
+  end
 end
-
-devise_for :users
