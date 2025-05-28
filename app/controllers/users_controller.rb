855a6a5a86e98cdd1acd62ba6c19dd11e7a35115
Alez05 <cercel.alexandru436@gmail.com>
2025-05-28 19:55:58 +0100

    feat: implemented the map, updates coming soon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @trips = Trip.geocoded.includes(:user)
+
+    @markers = @trips.geocoded.map do |trip|
+      {
+        lat: trip.latitude,
+        lng: trip.longitude
+      }
+    end
+  end
   def show
     @user = User.find(params[:id])
   end
