741e4f44521fbbdcb508830446bdb602559f6962
Aleksandra Wilk <aleksandra_wilk@yahoo.com>
2025-05-29 19:14:35 +0200

    uc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,23 +14,6 @@ class UsersController < ApplicationController
     end
   end
 
-   def index
-    @users = User.all
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user, notice: 'Profile updated successfully!'
-    else
-      render :edit
-    end
-  end
-
   private
 
   def username_param
@@ -69,6 +52,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @trips = @user.trips
   end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile updated successfully!'
+    else
+      render :edit
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:username, :email, :user_image)
