a2eae9a68593a7d1412de3e0e59afa401ceff0c3
Jordan Gilbert <jordan.gilbert@hotmail.co.uk>
2025-05-29 19:37:12 +0100

    all prompts for ai are saved and displayed on show page

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,8 @@ class Trip < ApplicationRecord
   has_many :trip_activities
   has_many :activities, through: :trip_activities
 
+  validates :start_date, :end_date, :location, :budget, presence: true
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
