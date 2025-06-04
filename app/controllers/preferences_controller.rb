cf7dfc07fec01151cab89854097522209120de21
Diego Colina <diego_col710@hotmail.com>
2025-06-04 15:22:46 +0100

    started my preferences controller

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,4 +1,31 @@
 class PreferencesController < ApplicationController
   def index
   end
+
+  def show
+    @preference = Preference.find(params[:id])
+  end
+
+  def new
+    @preference = Preference.new
+  end
+
+  def create
+    @preference = Preference.new(preference_params)
+    if @preference.save
+      redirect_to @preference, notice: 'Preference was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @preference = Preference.find(params[:id])
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:user_id, :key, :value)
+  end
 end
