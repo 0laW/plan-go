6947bdfa104464bd055c98cc9fbab1035a7a2c42
Aleksandra Wilk <aleksandra_wilk@yahoo.com>
2025-06-05 15:11:17 +0200

    added turbo frame + made changes to controller + routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_04_152931) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_04_152405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_04_152931) do
     t.float "latitude"
     t.float "longitude"
     t.string "image_url"
-    t.string "price_level"
     t.string "cost"
+    t.string "price_level"
     t.index ["category_id"], name: "index_activities_on_category_id"
   end
 
